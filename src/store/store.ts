8d1c366ddfbe00feb4795f7e85ab6467e24bb082
Solomon Barayev <sbarayev2@gmail.com>
2025-11-13 16:00:47 +0200

    Update Redux store configuration to include formData and formDocuments slices for improved state management

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { api } from './api/api'
 import constantsSliceReducer from './slices/constants.slice'
+import formDataSliceReducer from './slices/form-data.slice'
+import formDocumentsSliceReducer from './slices/form-documents.slice'
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     constants: constantsSliceReducer,
-    // formData: formDataSliceReducer,
+    formData: formDataSliceReducer,
+    formDocuments: formDocumentsSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
